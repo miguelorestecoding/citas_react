c8410776cfeb71f050f9738ee8a0d70bdb81e2ed
Miguel Oreste Capó <105470239+miguelorestecoding@users.noreply.github.com>
2024-02-21 09:54:27 -0300

    Primer y último commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,52 @@
-import { useState } from "react";
-import Formulario from "./components/Formulario";
-import Header from "./components/Header";
-import ListadoPacientes from "./components/ListadoPacientes";
+import { useState, useEffect } from 'react'
+import Formulario from "./components/Formulario"
+import Header from "./components/Header"
+import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
+
   const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({});
 
-  return (
-    <div className="container mx-auto mt-20">
-      <Header />
-      <div className="mt-12 md:flex">
-        <Formulario 
-        setPacientes={setPacientes}
-        />
-        <ListadoPacientes />
+    useEffect(() => {
+      const obtenerLS = () => {
+        const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+        setPacientes(pacientesLS)
+      }
+      obtenerLS();
+    }, []);
+
+    useEffect(() => {
+      localStorage.setItem('pacientes', JSON.stringify(pacientes));
+    }, [pacientes])
+
+    const eliminarPaciente = id => {
+      const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id);
+      setPacientes(pacientesActualizados)
+    }
+
+    return (
+      <div className="container mx-auto mt-20">
+        <Header />
+
+        <div className="mt-12 md:flex">
+          <Formulario
+            pacientes={pacientes}
+            setPacientes={setPacientes}
+            paciente={paciente}
+            setPaciente={setPaciente}
+          />
+          <ListadoPacientes
+            pacientes={pacientes}
+            setPaciente={setPaciente}
+            eliminarPaciente={eliminarPaciente}
+          />
+        </div>
+
       </div>
-    </div>
-  );
-}
+    )
+  }
 
-export default App;
+  export default App
+
+  
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,26 +1,49 @@
-const Paciente = () => {
+const Paciente = ({paciente, setPaciente, eliminarPaciente}) => {
+  const { nombre, propietario, email, fecha, sintomas, id } = paciente
+
+  const handleEliminar = () => {
+      const respuesta = confirm('Deseas eliminar este paciente?');
+
+      if(respuesta) {
+          eliminarPaciente(id)
+      }
+  }
+
   return (
-    <div className=" mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
-        <p className=" font-bold mb-3 text-gray-700 uppercase">
-          Nombre: {' '}
-          <span className="font-normal normal-case">Hook</span>
-        </p>
-        <p className=" font-bold mb-3 text-gray-700 uppercase">
-          Propietario: {' '}
-          <span className="font-normal normal-case">Juan</span>
-        </p>
-        <p className=" font-bold mb-3 text-gray-700 uppercase">
-          Email: {' '}
-          <span className="font-normal normal-case">correo@correo.com</span>
-        </p>
-        <p className=" font-bold mb-3 text-gray-700 uppercase">
-          Fecah de Alta: {' '}
-          <span className="font-normal normal-case">Fecha de Alta</span>
-        </p>
-        <p className=" font-bold mb-3 text-gray-700 uppercase">
-          Síntomas: {' '}
-          <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil quis sapiente voluptatibus vel maxime esse quae nulla labore est, ducimus temporibus sit ad in cumque minus nesciunt aut, rerum autem</span>
-        </p>
+      <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
+          <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+              <span className="font-normal normal-case">{nombre}</span>
+          </p>
+
+          <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+              <span className="font-normal normal-case">{propietario}</span>
+          </p>
+
+          <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+              <span className="font-normal normal-case">{email}</span>
+          </p>
+
+          <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+              <span className="font-normal normal-case">{fecha}</span>
+          </p>
+
+          <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+              <span className="font-normal normal-case">{sintomas}</span>
+          </p>
+
+          <div className="flex justify-between mt-10">
+              <button 
+                  type="button"
+                  className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                  onClick={() => setPaciente(paciente)}
+              >Editar</button>
+
+              <button 
+                  type="button"
+                  className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                  onClick={handleEliminar}
+              >Eliminar</button>
+          </div>
       </div>
   )
 }
